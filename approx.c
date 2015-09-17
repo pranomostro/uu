89f13bf3732f7759c29ed9b1ff9b5853fa277f47
scharlatan <a.regenfuss@gmx.de>
2015-09-17 22:28:05 +0200

    Worked on the algorithm.

--- approx.c
+++ approx.c
@@ -7,20 +7,29 @@ int afind(int key, int* data, int len);
 int main(void)
 {
 	int c, in, pos;
-	int* arr=(int*)calloc(16, sizeof(int));
+	int arr[16];
 
 	for(c=0; c<16; c++)
 	{
-		scanf("%i", &in);
+		scanf("%i\n", &in);
 		pos=afind(in, arr, c);
-		memmove(arr+1, arr, c*sizeof(int));
+		memmove(arr+c+1, arr+c, c*sizeof(int));
 		arr[pos]=in;
 	}
+
+	for(c=0; c<16; c++)
+		printf("%i\n", c);
 
 	return 0;
 }
 
 int afind(int key, int* data, int len)
 {
+	if(len<=0||data[0]>=key)
+		return 0;
+	else if (data[len-1]<key)
+		return len;
+
 	int low, mid, high;
+	return 0;
 }
